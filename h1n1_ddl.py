7092e4a83715640f26cef2432a54d4d2c66a1283
William Ruan <89553051+naurw@users.noreply.github.com>
2021-12-15 22:51:25 -0500

    Updated files
    Updated files with correct syntax

--- h1n1_ddl.py
+++ h1n1_ddl.py
@@ -42,14 +42,20 @@ Rerun meta.reflect & meta.tables.keys() to show that it is within the database:
 Base=declarative_base()
     
 h1n1trigger = DDL("""
-                  SELECT * FROM H1N1_Flu_Vaccines LIMIT 15;
-                  CREATE TRIGGER H1N1_concern_trigger BEFORE INSERT ON H1N1_Flu_Vaccines
-                  FOR EACH ROW BEGIN 
-                  IF NEW.alert >=3 THEN
-                  SIGNAL SQLSTATE '45000'
-                  SET MESSAGE_TEXT = 'H1N1 concern should be a numerical value between 0 and 3. Please try again.'
-                  ;END IF;
-                  END;
-                  """)
+delimiter $$
+
+CREATE TRIGGER H1N1_concern_trigger 
+BEFORE INSERT ON e2e.H1N1_Flu_Vaccines
+FOR EACH ROW
+BEGIN
+IF NEW.h1n1_concern >= 3 THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'H1N1 concern should be a numerical value between 0 and 3. Please try again.';
+END IF;
+END; 
+
+$$
+""")
 
 event.listen(Base.metadata, 'after_create', h1n1trigger)
+
--- h1n1_trigger.sql
+++ h1n1_trigger.sql
@@ -3,11 +3,15 @@ Create a trigger that pushes an error message to the user when a new row is inse
 The trigger should be called h1n1_concern_trigger and should have the following error message: “H1N1 concern should be a numerical value between 0 and 3. Please try again.”
 */
 
+delimiter $$
+
 CREATE TRIGGER H1N1_concern_trigger BEFORE INSERT ON e2e.H1N1_Flu_Vaccines
-FOR EACH ROW 
-BEGIN 
-IF NEW.h1n1_concern >=3 THEN
+FOR EACH ROW
+BEGIN
+IF NEW.h1n1_concern >= 3 THEN
 SIGNAL SQLSTATE '45000'
 SET MESSAGE_TEXT = 'H1N1 concern should be a numerical value between 0 and 3. Please try again.';
 END IF;
-END;
+END; 
+
+$$
